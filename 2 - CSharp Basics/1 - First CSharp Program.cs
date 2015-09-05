cbb3d28c892fbe3faa632309ca51f31057f040e1
Axel Prieto <prietoaxel@gmail.com>
2015-09-04 22:13:11 -0300

    Project started

---
+++ 2 - CSharp Basics/1 - First CSharp Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___CSharp_Basics
+{
+    class _1___First_CSharp_Program
+    {
+        static void Main()
+        {
+            int x = 12 * 30;
+            Console.WriteLine(x);
+        }
+    }
+}
